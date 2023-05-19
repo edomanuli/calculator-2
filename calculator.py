541c905199538dbaf09cbac37a534eca2119be81
Anuli Edom <edomanuli@gmail.com>
2023-05-19 13:21:16 -0500

    Implement the add condition

--- calculator.py
+++ calculator.py
@@ -1,17 +1,27 @@
 """CLI application for a prefix-notation calculator."""
 
-from arithmetic import (add, subtract, multiply, divide, square, cube,
-                        power, mod, )
+from arithmetic import (
+    add,
+    subtract,
+    multiply,
+    divide,
+    square,
+    cube,
+    power,
+    mod,
+)
 
 
 # Replace this with your code
 while True:
     # take user input
-    user_input = input('Type in the arithmetic problem here: ')
+    user_input = input("Type in the arithmetic problem here: ")
 
     # tokenize the input into a list eg: pow 4 5 => ['pow', 4, 5]
-    tokens = user_input.split(' ')
+    tokens = user_input.split(" ")
 
     # if the first input is 'q', then quit
-    if tokens[0] == 'q':
+    if tokens[0] == "q":
         break
+    elif tokens[0] == "+":  # create the add function with user input
+        print(add(int(tokens[1]), int(tokens[2])))
